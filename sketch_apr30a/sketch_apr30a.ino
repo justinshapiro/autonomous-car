824784cfdf2c92247f9aada77176f54c7533ee72
Justin Shapiro <justin.shapiro@ucdenver.edu>
2017-05-03 02:47:26 -0600

    Front laser sensor broke, replaced with the ultrasonic

--- sketch_apr30a/sketch_apr30a.ino
+++ sketch_apr30a/sketch_apr30a.ino
@@ -14,21 +14,21 @@ extern "C" {
 #define pinI3     12 //define IN3 interface - motor B
 #define pinI4     13 //define IN4 interface - motor B
 #define speedpinB 10 //enable motor B
-#define _speed1   200 // define the speed of motor A (left wheel)
-#define _speed2   200 // define the speed of motor B (right wheel)
+#define _speed1   120 // define the speed of motor A (left wheel)
+#define _speed2   120 // define the speed of motor B (right wheel)
 
 // Ultrasonic sensor
-//#define trigger 4
-//#define echo 5
+#define trigger 6
+#define echo 5
 
 // Laser Range sensors
 Adafruit_VL6180X lasers;
-byte sensor1_pin = 3; 
-byte sensor2_pin = 6;
-byte sensor3_pin = 5;
+byte sensor1_pin = 2; // right
+byte sensor2_pin = 6; // left
+byte sensor3_pin = 5; // front
 
-const int distanceThreshold = 100; // mm
-const int wallDistance = 100;
+const int distanceThreshold = 100; 
+const int wallDistance = 70;
 const int maxSensorDistance = 255;
 
 // Motor control routines
@@ -38,12 +38,12 @@ void forward() {
   analogWrite(speedpinB,_speed2);
 
   //turn DC Motor B move clockwise
-  digitalWrite(pinI4,HIGH);
-  digitalWrite(pinI3,LOW);
+  digitalWrite(pinI4,LOW);
+  digitalWrite(pinI3,HIGH);
 
   //turn DC Motor A move anticlockwise
-  digitalWrite(pinI2,LOW);
-  digitalWrite(pinI1,HIGH);
+  digitalWrite(pinI2,HIGH);
+  digitalWrite(pinI1,LOW);
 }
  
 void backward() {
@@ -52,32 +52,18 @@ void backward() {
   analogWrite(speedpinB,_speed2);
   
   //turn DC Motor B move anticlockwise
-  digitalWrite(pinI4,LOW);
-  digitalWrite(pinI3,HIGH);
+  digitalWrite(pinI4,HIGH);
+  digitalWrite(pinI3,LOW);
   
   //turn DC Motor A move clockwise
-  digitalWrite(pinI2,HIGH);
-  digitalWrite(pinI1,LOW);
+  digitalWrite(pinI2,LOW);
+  digitalWrite(pinI1,HIGH);
 }
  
 void left() {
   //input a simulation value to set the speed
   analogWrite(speedpinA,_speed1);       // need to make these speed values smaller if we 
   analogWrite(speedpinB,_speed2);       // want smaller turns (or really small and add a delay as necessary)
-  
-  //turn DC Motor B move clockwise
-  digitalWrite(pinI4,HIGH);
-  digitalWrite(pinI3,LOW);
-
-  //turn DC Motor A move clockwise
-  digitalWrite(pinI2,HIGH);
-  digitalWrite(pinI1,LOW);
-}
-
-void slightlyLeft(){
-  //input a simulation value to set the speed
-  analogWrite(speedpinA,200);
-  analogWrite(speedpinB,200);
   
   //turn DC Motor B move clockwise
   digitalWrite(pinI4,HIGH);
@@ -114,6 +100,30 @@ void slightlyRight(){
   //turn DC Motor A move clockwise
   digitalWrite(pinI2,LOW);
   digitalWrite(pinI1,HIGH);
+
+  delay(400);
+  forward();
+  delay(400);
+  stop();
+}
+
+void slightlyLeft(){
+  //input a simulation value to set the speed
+  analogWrite(speedpinA,_speed1);
+  analogWrite(speedpinB,_speed2);
+  
+  //turn DC Motor B move clockwise
+  digitalWrite(pinI4,HIGH);
+  digitalWrite(pinI3,LOW);
+
+  //turn DC Motor A move clockwise
+  digitalWrite(pinI2,HIGH);
+  digitalWrite(pinI1,LOW);
+
+  delay(400);
+  forward();
+  delay(400);
+  stop();
 }
  
 void stop() {
@@ -145,14 +155,24 @@ int getLeft() {
 }
 
 int getFront() {
-  delay(50);
-  digitalWrite(sensor1_pin, LOW); 
-  digitalWrite(sensor2_pin, LOW);
-  digitalWrite(sensor3_pin, HIGH); 
-  lasers.begin();
-  delay(1);
-  Serial.println("Front: " + String(lasers.readRange()));
-  return lasers.readRange();
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  short duration = pulseIn(echo, HIGH);
+  Serial.println("Front: " + String((5 * duration) / 29.1));
+  return abs((5 * duration) / 29.1);
+}
+
+void go90Left() {
+  left();
+  delay(400);  
+}
+
+void go90Right() {
+  right();
+  delay(400);  
 }
 
 // Car control routine
@@ -160,8 +180,8 @@ void setup() {
   Serial.begin(9600);
 
   // Ultrasonic sensor
- // pinMode(trigger, OUTPUT);
-  //pinMode(echo, INPUT);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
 
   // Motor
   pinMode(pinI1,OUTPUT);
@@ -174,80 +194,37 @@ void setup() {
   // Laser Range sensor
   pinMode(sensor1_pin, OUTPUT);
   pinMode(sensor2_pin, OUTPUT);
-  pinMode(sensor3_pin, OUTPUT);
+  //pinMode(sensor3_pin, OUTPUT);
   digitalWrite(sensor1_pin, LOW);
   digitalWrite(sensor2_pin, LOW);
-  digitalWrite(sensor3_pin, LOW); 
+  // digitalWrite(sensor3_pin, LOW); 
   Wire.begin();
 
   // Start car
-  //forward();
+  forward();
 }
 
 void loop() {
-  if (wallDistance > getRight()){       // if we need to adjust to the left
-    slightlyLeft();
-  }
-  if (getLeft() < wallDistance){        // if we need to adjust to the right
-    slightlyRight();
-  }
-  
-  while (getFront() <= distanceThreshold) {   // not sure if this should be a while anymore
-    //backward();                             // I think it'll be easier to just catch the wall soon enough
-    stop();                                   // than to require a constant check for backing up
-    if (getRight() > 100){                    // 100 is kind of arbitrary, will need to fine tune thisl
-      right();
-      delay(400);         // can use the same function for right and slightlyright and just add a delay 
-    }                     // to specify how much (or how long) we want to turn (for)
-    if (getLeft() > 100){
-      left();
-      delay(400);         // 400 worked for 90 degree turns on hard floor at 200/200 speed on fresh batteries
-    }
-    // maybe store left and right distances in local variables
-    // and compare them to make a turn decision to prevent both if's from running
-    
-    /*
-    while (getRight() <= distanceThreshold) {
-      left();
-    }
-    while (getLeft() <= distanceThreshold){
-      right();
-    }
-    */
-  }
-  forward();
-  /*
-  if (wallDistance > getRight()){
-    slightlyLeft();
-  }
-  if (getLeft() < wallDistance){
-    slightlyRight();
-  }
-  
-  while (getFront() <= distanceThreshold) {
-    backward();
-    if (getRight() > maxSensorDistance){
-      right();
-    }
-    if (getLeft() > maxSensorDistance){
-      left();
-    }
-    while (getRight() <= distanceThreshold) {
-      left();
-    }
-    while (getLeft() <= distanceThreshold){
-      right();
-    }
-  }
-  forward();
- */ 
-  //while (getLeft() <= distanceThreshold){
-  //  right();
-  //}
-  //forward();
+  int _front = getFront();
+  int _left = getLeft();
+  int _right = getRight();
 
-  //while (getRight() <= distanceThreshold) {
-  //  left();
-  //}
-  //forward();
+
+  // Handle the event that something is in the front
+  if (_front <= distanceThreshold) {
+    stop();
+    if (_left > wallDistance) {
+      go90Left();
+    } else if (_right > wallDistance) {
+      go90Right();
+    }
+  }
+
+  forward();
+  if (getLeft() <= wallDistance) {
+    slightlyRight();
+  }
+  if (getRight() <= wallDistance) {
+    slightlyLeft();
+  }
 }
