5faf08c4c5dd21db4df5c2be63ba1317fbbf80a1
Justin Shapiro <justin.shapiro@ucdenver.edu>
2017-05-03 16:32:59 -0600

    states added

--- sketch_apr30a/sketch_apr30a.ino
+++ sketch_apr30a/sketch_apr30a.ino
@@ -14,8 +14,8 @@ extern "C" {
 #define pinI3     12 //define IN3 interface - motor B
 #define pinI4     13 //define IN4 interface - motor B
 #define speedpinB 10 //enable motor B
-#define _speed1   120 // define the speed of motor A (left wheel)
-#define _speed2   120 // define the speed of motor B (right wheel)
+#define _speed1   150 // define the speed of motor A (left wheel)
+#define _speed2   150 // define the speed of motor B (right wheel)
 
 // Ultrasonic sensor
 #define trigger 6
@@ -27,7 +27,7 @@ byte sensor1_pin = 2; // right
 byte sensor2_pin = 6; // left
 
 const int distanceThreshold = 255; 
-const int wallDistance = 50;
+const int wallDistance = 40;
 int _front;
 int _left;
 int _right;
@@ -49,11 +49,11 @@ void forward() {
   digitalWrite(pinI2,HIGH);
   digitalWrite(pinI1,LOW);
 
-  if (getLeft() < wallDistance) {
+  if (getLeft() <= wallDistance) {
     slightlyRight();
   }
 
-  if (getRight() < wallDistance) {
+  if (getRight() <= wallDistance) {
     slightlyLeft();
   }
 }
@@ -113,7 +113,8 @@ void slightlyRight(){
   digitalWrite(pinI2,LOW);
   digitalWrite(pinI1,HIGH);
 
-  delay(200);
+  delay(200); 
+  stop();
 }
 
 void slightlyLeft(){
@@ -130,12 +131,13 @@ void slightlyLeft(){
   digitalWrite(pinI1,LOW);
 
   delay(200);
+  stop();
 }
  
 void stop() {
   digitalWrite(speedpinA,LOW); 
   digitalWrite(speedpinB,LOW);
-  delay(2000);
+  delay(100);
 }
 
 int getRight() {
@@ -144,7 +146,7 @@ int getRight() {
   digitalWrite(sensor1_pin, HIGH); 
   lasers.begin(); 
   delay(1);
-  Serial.println("Right: " + String(lasers.readRange()));
+  // Serial.println("Right: " + String(lasers.readRange()));
   return lasers.readRange();
 }
 
@@ -154,7 +156,7 @@ int getLeft() {
   digitalWrite(sensor2_pin, HIGH); 
   lasers.begin();
   delay(1);
-  Serial.println("Left: " + String(lasers.readRange()));
+  // Serial.println("Left: " + String(lasers.readRange()));
   return lasers.readRange();
 }
 
@@ -165,42 +167,46 @@ int getFront() {
   delayMicroseconds(10);
   digitalWrite(trigger, LOW);
   short duration = pulseIn(echo, HIGH);
-  int result = abs((5 * duration) / 29.1);
-  if (result > 255 || result == 0) {
+  int result = abs((5 * duration) / 29.1) - 10;
+  if (result > 255) {
     result = 255;
   }
-  Serial.println("Front: " + String(result));
   return result;
 }
 
 void go90Left() {
   left();
-  delay(400);  
+  delay(750);  
 }
 
 void go90Right() {
   right();
-  delay(400);  
+  delay(750);  
 }
 
 int getState() {
-  _front = getFront();
+  _front = 0;
+  for (int i = 0; i < 5; i++) {
+    _front += getFront();
+  }
+  _front /= 5;
+  Serial.println("Front: " + String(_front));
   _right = getRight();
   _left = getLeft();
 
-  if (_front > wallDistance) {
+  if (_front >= wallDistance) {
     front_free = true;
   } else {
     front_free = false;
   }
 
-  if (_left > distanceThreshold) {
+  if (_left >= distanceThreshold) {
     left_free = true;
   } else {
     left_free = false;
   }
 
-  if (_right > distanceThreshold) {
+  if (_right >= distanceThreshold) {
     right_free = true;
   } else {
     right_free = false;
@@ -208,16 +214,18 @@ int getState() {
 
   if (front_free && !left_free && !right_free) {
     return 0;
-  } else if (front_free && left_free && !right_free) {
-    return 0;
   } else if (front_free && left_free && right_free) {
     return 0;
+  } else if (front_free && !left_free && right_free) {
+    return 1;
   } else if (!front_free && left_free && right_free) {
     return 1;
   } else if (!front_free && !left_free && right_free) {
     return 1;
   } else if (!front_free && !left_free && !right_free) {
     return 2;
+  } else if (front_free && left_free && !right_free) {
+    return 3;
   } else if (!front_free && left_free && !right_free) {
     return 3;
   } 
@@ -246,33 +254,43 @@ void setup() {
   digitalWrite(sensor2_pin, LOW);
   Wire.begin();
 
-  delay(5000);
+  delay(2500);
   // Start car
   forward();
 }
 
 void loop() {
   int state = getState();
+ 
   switch (state) {
     case 0: {
       // Move forward
       forward();
+      delay(1000);
     } break;
     case 1: {
       // Move right, then forward
+      backward();
+      delay(500);
       go90Right();
       forward();
+      delay(2000);
     } break;
     case 2: {
       // Turn around, then forward
+      backward();
+      delay(500);
       go90Right();
       go90Right();
       forward();
+      delay(500);
     } break;
     case 3: {
       // Move left, then forward
+      delay(500);
       go90Left();
       forward();
+      delay(2000);
     } break;
     default: break;
   }
